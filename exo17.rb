cefac1ade7cdcea49de9a7cbac74b01d28cc355f
Axeloow <axelouli45@gmail.com>
2023-04-11 10:53:50 +0200

    Ajout de l'exercice 17. Let'sa goww !

---
+++ exo17.rb
@@ -0,0 +1,20 @@
+puts "Salut ! Bienvenue dans la pyramide du maçon ! Combien d'étages veux-tu construire ?"
+print "> "
+etage = gets.to_i
+puts "#{etage} etages ? Let'sa gowwwwww!"
+ligne = 1
+espace = 5
+etage.times do
+  espace.times do
+    print " "
+  end
+  ligne.times do
+  print "#"
+  end
+  espace.times do
+    print " "
+  end
+  ligne = ligne+2
+  espace = espace-1
+  puts
+end
